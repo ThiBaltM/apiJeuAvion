6990e0e4da53a5f40d417327365ea052377536d5
thibalt.meurin@ThiBalt <thibalt.meurin@laposte.net>
2023-12-08 09:15:07 +0100

    first commit

---
+++ src/Entity/Score.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+
+#[ORM\Entity(repositoryClass: ScoreRepository::class)]
+#[ApiResource()]
+#[ApiFilter(OrderFilter::class, properties: ['time' => 'ASC', 'name' => 'ASC'])]
+class Score
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $time = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTime(): ?float
+    {
+        return $this->time;
+    }
+
+    public function setTime(float $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
